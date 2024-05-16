10e339e267402955a47e0b6672f3da71feeb5e21
BIKRAM NAHAK <bikramnahak9714@gmail.com>
2024-05-16 19:38:12 +0530

    react-project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Card from './component/card/card'
+
+const App = () => {
+
+  return (
+    <div className='container'>
+      <div className="row row-gap">
+        <Card />
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
